490528d776ba04a68f33588989dcebbb0e8a4dcf
maytom2016 <qwerty448qwe@163.com>
2024-05-02 21:05:17 +0800

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -21,8 +21,8 @@ export async function onRequestPost(context) {  // Contents of context object
      next, // used for middleware or to fetch assets    
      data, // arbitrary space for passing data between middlewares 
      } = context;
-     console.log("requesheader:"+request.headers)
-     if(request.headers['authCode'] == env.AUTH_CODE){
+     console.log("requesheader:"+request.headers.get('authCode'))
+     if(request.headers.get('authCode') == env.AUTH_CODE){
      context.request
      const url = new URL(request.url);
      const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
