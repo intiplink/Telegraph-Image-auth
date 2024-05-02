d809bcf1aeb2be42b14024c0272b1d69742274fe
maytom2016 <qwerty448qwe@163.com>
2024-05-02 18:08:57 +0800

    1
    1

---
+++ functions/upload.js
@@ -0,0 +1,42 @@
+function UnauthorizedException(reason) {
+  return new Response(reason, {
+    status: 401,
+    statusText: "Unauthorized",
+    headers: {
+      "Content-Type": "text/plain;charset=UTF-8",
+      // Disables caching by default.
+      "Cache-Control": "no-store",
+      // Returns the "Content-Length" header for HTTP HEAD requests.
+      "Content-Length": reason.length,
+    },
+  });
+}
+
+export async function onRequestPost(context) {  // Contents of context object  
+    const {   
+     request, // same as existing Worker API    
+     env, // same as existing Worker API    
+     params, // if filename includes [id] or [[path]]   
+     waitUntil, // same as ctx.waitUntil in existing Worker API    
+     next, // used for middleware or to fetch assets    
+     data, // arbitrary space for passing data between middlewares 
+     } = context;
+     if(1==1){
+     context.request
+     const url = new URL(request.url);
+     const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
+         method: request.method,
+         headers: request.headers,
+         body: request.body,
+     });
+     console.log("pathname"+"url.pathname");
+     console.log("search"+"url.search");
+     console.log(env);
+     console.log(params.code);
+    return response;
+     }
+     else
+     {
+      return new UnauthorizedException("error");
+     }
+  }
