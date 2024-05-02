d685410d1c2636d9b8de94c53ac701a7922c26c4
maytom2016 <qwerty448qwe@163.com>
2024-05-02 15:03:20 +0800

    Update b94451d72c8d5d219157.js
    1

--- _nuxt/b94451d72c8d5d219157.js
+++ _nuxt/b94451d72c8d5d219157.js
@@ -1,37 +1,25 @@
-export async function onRequestPost(context) {
-    // Contents of context object
-    const {
-      request, // same as existing Worker API
-      env, // same as existing Worker API
-      params, // if filename includes [id] or [[path]]
-      waitUntil, // same as ctx.waitUntil in existing Worker API
-      next, // used for middleware or to fetch assets
-      data, // arbitrary space for passing data between middlewares
-    } = context;
-    context.request;
-    const url = new URL(request.url);
-	  return decodeURIComponent(params.authCode);
-    if (decodeURIComponent(params.authCode) === env.AUTH_CODE) {
-        const response = fetch("https://telegra.ph/" + url.pathname + url.search, {
-        method: request.method,
-        headers: request.headers,
-        body: request.body,
-        });
-        return response;
-    }else {
-        return new UnauthorizedException('no auth')
+var requestOptions = {
+  method: "GET",
+  redirect: "follow",
+  //include authorization credientials
+  credentials: "include",
+};
+const authCode =
+  window.localStorage.getItem("authCode") || window.prompt("authCode");
+fetch(`./api/authCode/${authCode}`)
+  .then(async (res) => {
+    const isOK = await res.text();
+    if (isOK === "ok") {
+      window.localStorage.setItem("authCode", authCode);
+    } else {
+      window.location.reload()
+      console.log("status", status);
     }
-  }
-  function UnauthorizedException(reason) {
-    return new Response(reason, {
-      status: 401,
-      statusText: "Unauthorized",
-      headers: {
-        "Content-Type": "text/plain;charset=UTF-8",
-        // Disables caching by default.
-        "Cache-Control": "no-store",
-        // Returns the "Content-Length" header for HTTP HEAD requests.
-        "Content-Length": reason.length,
-      },
-    });
-  }
+  })
+  .catch((e) => {
+    window.localStorage.removeItem("authCode")
+    console.log(" ----");
+    console.log("e:", e);
+    console.log(" ----");
+    window.location.reload();
+  });
