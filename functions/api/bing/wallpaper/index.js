52e47929c7f803b3ebff476253723a4a53c343f7
intiplink <w3d03sss@yahoo.com>
2024-07-04 13:53:30 +0700

    TEST

--- functions/api/bing/wallpaper/index.js
+++ functions/api/bing/wallpaper/index.js
@@ -8,11 +8,11 @@ export async function onRequest(context) {
       next, // used for middleware or to fetch assets
       data, // arbitrary space for passing data between middlewares
     } = context;
-    const res = await fetch(`https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=5`);
+    const res = await fetch(`https://bing.com/HPImageArchive.aspx?format=js&idx=0&n=5`);
     const bing_data = await res.json();
     const return_data={
         "status":true,
-        "message":"操作成功",
+        "message":"The operation is successful.",
         "data": bing_data.images
     }
     const info = JSON.stringify(return_data);
